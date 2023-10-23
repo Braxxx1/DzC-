a6d9a61a59d1fe81bf1d0bd8d2338310374fb8bc
Braxxx1 <pavel120304@yandex.ru>
2023-10-23 13:07:07 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Задание 1
+// int a = Convert.ToInt32(Console.ReadLine()) / 10; 
+// Console.WriteLine(a % 10); 
+
+// Задание 2
+// string a = Console.ReadLine()!;
+// int n;
+// bool pr = int.TryParse(a, out n); 
+// if (a.Length >= 3 && pr){ 
+//     // Console.WriteLine(Convert.ToString(n)[2]); ну можно вот так
+//     while (Convert.ToString(n).Length > 3){
+//         n/=10;
+//     } 
+//     Console.WriteLine(n % 10); 
+// } 
+// else{ 
+//     Console.WriteLine("Третьей цифры нет"); 
+// } 
+ 
+// Задание 3
+// Console.Write("введите число (день недели) от 1 до 7: "); 
+// int a = Convert.ToInt32(Console.ReadLine()); 
+// if ((a == 6) || (a == 7)){ 
+//     Console.WriteLine("да"); 
+// } 
+// else if (a > 7) { 
+//     Console.WriteLine("Это не день недели, введите число (день недели) от 1 до 7"); 
+// } 
+// else{ 
+//     Console.WriteLine("нет"); 
+// } 
+ 
